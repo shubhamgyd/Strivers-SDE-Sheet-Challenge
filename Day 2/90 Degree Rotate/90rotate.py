c7480e573cb8522f313ecc00caf49ec3369ea72a
gaurav yadav <neeta6854@gmail.com>
2023-08-31 01:46:29 +0530

    Does small formatting

--- Day 2/90 Degree Rotate/90rotate.py
+++ Day 2/90 Degree Rotate/90rotate.py
@@ -1,13 +1,3 @@
-# def rotate90degree(matrix):
-#     n = len(matrix)
-#     temp = [[0 for _ in range(n)] for _ in range(n)]
-#     for i in range(3):
-#         temp[i][2] = matrix[0][i]
-#     for i in range(3):
-#         temp[i][1] = matrix[1][i]
-#     for i in range(3):
-#         temp[i][0] = matrix[2][i]
-#     return temp
 def rotate90degree(matrix,n,m):
     n = len(matrix)
     # transposing the matrix
