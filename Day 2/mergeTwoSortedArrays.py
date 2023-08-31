6c6ea68da9fc31989e1885ff643f76c497e15021
Striver <neeta6854@gmail.com>
2023-08-31 11:05:23 +0530

    mergeTwo sorted arrays brute force approach is done

---
+++ Day 2/mergeTwoSortedArrays.py
@@ -0,0 +1,27 @@
+# Problem statement: 
+# Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order.
+# Merge them in sorted order. Modify arr1 so that it contains the first N elements and modify arr2
+# so that it contains the last M elements.
+
+def mergeSorted(arr1, arr2):
+    n = len(arr1)
+    m = len(arr2)
+    ptr1 = 0
+    ptr2 = 0
+    while ptr1 < n:
+        if arr1[ptr1] > arr2[ptr2]:
+            arr1[ptr1],arr2[ptr2] = arr2[ptr2],arr1[ptr1]
+            print(arr2[ptr2])
+            if arr2[ptr2] > arr2[ptr2+1]:
+                arr2.sort()
+        else:
+            ptr1 += 1
+
+arr1 = [1,4,8,10,11]
+arr2 = [2,3,9]
+mergeSorted(arr1,arr2)
+print(arr1)
+print(arr2)
+
+# Time Complexity: O(nlogn + n)
+# Space Complexity: O(1)
