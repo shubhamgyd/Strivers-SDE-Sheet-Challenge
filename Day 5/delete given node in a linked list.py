cc441e12a23160254e0dc8b1759c434db0525b79
Striver <shubhamyadav97660@gmail.com>
2023-09-10 13:34:50 +0530

    delete given node in a linked list coded successfully and Day 4 completed successfully

---
+++ Day 5/delete given node in a linked list.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, k):
+        self.key = k
+        self.next = None
+
+def printLinkedList(head):
+    curr = head
+    while curr != None:
+        print(curr.key)
+        curr = curr.next
+
+def delete(temp):
+    temp.key = temp.next.key
+    temp.next = temp.next.next
+    
+          
+head = Node(1)
+head.next = Node(4)
+head.next.next = Node(2)
+head.next.next.next = Node(3)
+
+printLinkedList(head)
+delete(head.next.next)
+printLinkedList(head)
