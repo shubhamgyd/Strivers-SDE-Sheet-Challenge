afbdcb0e8e1ca102ba4b7b6c936c4198ccdd9af0
Striver <shubhamyadav97660@gmail.com>
2023-09-19 22:22:15 +0530

    modified nextPermutation.py

--- Day 1/nextPermutation.py
+++ Day 1/nextPermutation.py
@@ -5,7 +5,7 @@ def nextGreaterPermutation(A):
         if A[i] < A[i+1]:
             ind = i
             break
-    
+    print(ind)
     if ind == -1:
         A.reverse()
         return A
@@ -13,11 +13,15 @@ def nextGreaterPermutation(A):
     for i in range(n-1, ind, -1):
         if A[i] > A[ind]:
             A[i],A[ind] = A[ind],A[i]
+            break
+    # A[ind+1],A[ind] = A[ind], A[ind+1]
             
     # Step 3: reverse the right half:
     A[ind+1:] = reversed(A[ind+1:])
+    print(A[ind+1:])
     
     return A
 
 if __name__=="__main__":
-    print(nextGreaterPermutation([1,2,3]))
+    # print(nextGreaterPermutation([1,2,3]))
+    print(nextGreaterPermutation([2,1,5,4,3,0,0]))
