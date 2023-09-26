ea4f496f99d2de71ba2c43d5039de072999587c3
Striver <shubhamyadav97660@gmail.com>
2023-09-26 20:42:00 +0530

    Frog Jump problem understood successfully

---
+++ Dynamic Programming/frog jump/tabulation frog jump.py
@@ -0,0 +1,15 @@
+import sys
+def frogJump(arr,n):
+    dp = [-1 for i in range(n)]
+    dp[0] = 0
+    for index in range(1,n):
+        fs = dp[index - 1] + abs(arr[index] - arr[index - 1])
+        ss = sys.maxsize
+        if index > 1:
+            ss = dp[index - 2] + abs(arr[index] - arr[index-2])
+        dp[index] = min(fs,ss)
+    return dp[n-1]
+
+arr = [30,10,60,10,60,50]
+n = len(arr)
+print(frogJump(arr,n))
