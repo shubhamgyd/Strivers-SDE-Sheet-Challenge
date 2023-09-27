1273842474d8e74ed1cdd87bfac0b456d08969e2
shubhamgyd <108620698+shubhamgyd@users.noreply.github.com>
2023-09-27 17:31:06 +0530

    Update Set Matrix Zero.py

--- Day 1/Set Matrix Zero.py
+++ Day 1/Set Matrix Zero.py
@@ -16,3 +16,56 @@ def setZero(matrix, n, m):
 matrix = [[2,4,3],[1,0,0]]
 setZero(matrix,2,3)
 print(matrix)
+
+"""
+
+def zeroMatrix(matrix, n, m):
+    # int row[n] = {0}; --> matrix[..][0]
+    # int col[m] = {0}; --> matrix[0][..]
+
+    col0 = 1
+    # step 1: Traverse the matrix and
+    # mark 1st row & col accordingly:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                # mark i-th row:
+                matrix[i][0] = 0
+
+                # mark j-th column:
+                if j != 0:
+                    matrix[0][j] = 0
+                else:
+                    col0 = 0
+
+    # Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+    for i in range(1, n):
+        for j in range(1, m):
+            if matrix[i][j] != 0:
+                # check for col & row:
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+    #step 3: Finally mark the 1st col & then 1st row:
+    if matrix[0][0] == 0:
+        for j in range(m):
+            matrix[0][j] = 0
+    if col0 == 0:
+        for i in range(n):
+            matrix[i][0] = 0
+
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
+""""
